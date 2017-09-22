de1011044d31fa44f3821feae36300c3b89a04a3
Job Joseph <job_joseph@hotmail.com>
2017-09-22 15:12:06 -0500

    Retrieving object and bytes32 data

---
+++ src/NethereumClient/Question.cs
@@ -0,0 +1,28 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NethereumClient
+{
+    [FunctionOutput]
+    public class Question
+    {
+        [Parameter("uint",1)]
+        public int questionIndex1 { get; set; }
+
+        [Parameter("string", 2)]
+        public string questionId { get; set; }
+
+        [Parameter("uint", 3)]
+        public int questionTextRows { get; set; }
+
+        [Parameter("string",4)]
+        public string boardRecommendation { get; set; }
+
+        [Parameter("uint",5)]
+        public int isActive { get; set; }
+
+    }
+}
